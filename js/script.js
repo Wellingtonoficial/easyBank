65d4266527d57587d3e5ea8c5b52cd17e70a7162
Wellington <wellingtonoficial00@gmail.com>
2023-02-02 01:48:40 -0300

    first

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let btnMenu = document.querySelector(".menuHamburguer")
+let menu = document.querySelector(".menu")
+let imgHamburguer = document.querySelector('.hamburguer')
+
+btnMenu.addEventListener('click', ()=>{
+    menu.classList.toggle('activer')
+
+    menu.classList.contains('activer')? imgHamburguer.src = "../images/icon-close.svg":imgHamburguer.src = "../images/icon-hamburger.svg"
+})
