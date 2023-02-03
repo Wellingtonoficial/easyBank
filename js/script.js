8f8fa4613a12b30c613de5ac02bea58465a67858
Wellington <wellingtonoficial00@gmail.com>
2023-02-02 21:27:45 -0300

    .

--- js/script.js
+++ js/script.js
@@ -5,5 +5,5 @@ let imgHamburguer = document.querySelector('.hamburguer')
 btnMenu.addEventListener('click', ()=>{
     menu.classList.toggle('activer')
 
-    menu.classList.contains('activer')? imgHamburguer.src = "../images/icon-close.svg":imgHamburguer.src = "../images/icon-hamburger.svg"
+    menu.classList.contains('activer')? imgHamburguer.src = "./images/icon-close.svg":imgHamburguer.src = "./images/icon-hamburger.svg"
 })
